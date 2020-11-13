a3d71c8685b7f7301ecc605989dbdbf367262919
Mray000 <ainurhabibullin0@gmail.com>
2020-11-14 02:14:15 +0300

    styling 2 compomemts

--- src/Components/Messanger2/PhotoPreviewModal.jsx
+++ src/Components/Messanger2/PhotoPreviewModal.jsx
@@ -1,80 +1,92 @@
 import React from "react";
-
+import _ from "lodash";
 import { Form, Formik, Field } from "formik";
 import { Button, Modal } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { AddMessage } from "../../Redux/Reducer/DialogsReducer";
 import "./Messanger.scss";
+import { TextField } from "formik-material-ui";
 
 const PhotoPreviewModal = ({ show, src, handleClose, mobile = false }) => {
   const dispatch = useDispatch();
   return (
-    <div className="photo_prview_modal_global_container">
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="preview_photo_modal_body">
-          <div>
-            <img src={src} alt="" className="w-50" />
+    <Modal
+      show={show}
+      onHide={handleClose}
+      className="photo_preview_modal_global_container"
+    >
+      {/* <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header> */}
+      <Modal.Body className="preview_photo_modal_body">
+        <div className="photo_preview_modal_images_container">
+          <div className="photo_preview_modal_image_container">
+            <img src={src} alt="" />
           </div>
-          <Formik
-            onSubmit={(values, actions) => {
-              let message = values.preview_message.split("");
-              if (message.length > 30) {
-                let countDel = Math.ceil(message.length / 30);
-                let i = 1;
-                while (i <= countDel + 1) {
-                  if (i !== countDel + 1) message.insert(i * 30 - i - 1, "\n");
-                  else message.insert(i * 30 - i, "\n");
-                  i++;
-                }
-                message = message.join("");
-                dispatch(AddMessage(message, src));
-              } else dispatch(AddMessage(message ? message : null, src));
-              actions.resetForm();
-            }}
-            initialValues={{ preview_message: "" }}
-          >
-            {({ handleSubmit, values }) => (
-              <Form
-                onSubmit={handleSubmit}
-                className={`preview_add_message_in_${
-                  mobile ? "mobile_" : ""
-                }container`}
-              >
-                <Field
-                  name="preview_message"
-                  wrap="hard"
-                  placeholder="type..."
-                  as="textarea"
-                  className="add_message_in_preview"
-                  onKeyPress={(event) => {
-                    if (event.key === "Enter" && !event.shiftKey) {
-                      event.preventDefault();
-                    }
+          {/* <div className="photo_preview_modal_image_container">
+            <img src={src} alt="" className="photo_preview_modal_image" />
+          </div> */}
+        </div>
+        <Formik
+          onSubmit={(values, actions) => {
+            let message = values.preview_message.split("");
+            if (message.length > 30) {
+              let countDel = Math.ceil(message.length / 30);
+              let i = 1;
+              while (i <= countDel + 1) {
+                if (i !== countDel + 1) message.insert(i * 30 - i - 1, "\n");
+                else message.insert(i * 30 - i, "\n");
+                i++;
+              }
+              message = message.join("");
+              dispatch(AddMessage(message, src));
+            } else dispatch(AddMessage(message ? message : null, src));
+            actions.resetForm();
+          }}
+          initialValues={{ preview_message: "" }}
+        >
+          {({ handleSubmit, values }) => (
+            <Form
+              onSubmit={handleSubmit}
+              className={`preview_add_message_in_${
+                mobile ? "mobile_" : ""
+              }container`}
+            >
+              <Field
+                component={TextField}
+                multiline
+                color="white"
+                currentColor="white"
+                name="preview_message"
+                placeholder="type..."
+                className="preview_add_message_in"
+                onKeyPress={(event) => {
+                  if (event.key === "Enter" && !event.shiftKey) {
+                    handleSubmit();
+                    event.preventDefault();
+                    handleClose();
+                  }
+                }}
+              />
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    handleSubmit();
+                    handleClose();
                   }}
-                />
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button
-                    variant="primary"
-                    onClick={() => {
-                      handleSubmit();
-                      handleClose();
-                    }}
-                  >
-                    Send
-                  </Button>
-                </Modal.Footer>
-              </Form>
-            )}
-          </Formik>
-        </Modal.Body>
-      </Modal>
-    </div>
+                >
+                  Send
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )}
+        </Formik>
+      </Modal.Body>
+    </Modal>
   );
 };
 
