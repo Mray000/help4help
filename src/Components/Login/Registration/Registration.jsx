06a3a52b97685eb77fbeddc8bb653fbfae741987
Mray000 <ainurhabibullin0@gmail.com>
2020-12-16 00:02:09 +0300

    registrationnn

--- src/Components/Login/Registration/Registration.jsx
+++ src/Components/Login/Registration/Registration.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Formik } from "formik";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -8,93 +8,119 @@ import DnD from "./DnD";
 
 const Registration = () => {
   const mobile = false;
-
-  if (mobile) return <Redirect to="/profile" />;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [fade, setFade] = useState(false);
   return (
     <Formik onSubmit={console.log} initialValues={Object}>
-      {({ handleSubmit, handleChange, touched, errors, values }) => (
+      {({
+        handleSubmit,
+        handleChange,
+        touched,
+        errors,
+        values,
+        isValidating,
+      }) => (
         <Form
           onSubmit={handleSubmit}
-          id={`g_global_registration_${mobile ? "mobile_" : ""}container`}
+          className={`g_global_registration_${
+            mobile ? "mobile_" : ""
+          }container `}
         >
           <div className="col-4"></div>
           <Form.Group
-            id={`global_registration_${mobile ? "mobile_" : ""}container`}
-            className="col-4"
+            className={`global_registration_${
+              mobile ? "mobile_" : ""
+            }container  col-4`}
           >
-            <div className={`registration_${mobile ? "mobile_" : ""}form`}>
-              <div className="registration_form__title">
-                <h1 className="title_content">Registration</h1>
-              </div>
-              <div className="registration_form_controls">
-                <Form.Group className="form_group">
-                  <Form.Label className="in__label">Email</Form.Label>
-                  <Form.Control
-                    required
-                    type="email"
-                    name="email"
-                    onChange={handleChange}
-                    className="registration_form_in"
-                    isValid={touched.email && values.email}
-                    // isInvalid={touched.email && email(values.email)}
-                  />
-                </Form.Group>
+            {pageNumber === 1 ? (
+              <div
+                className={`registration_${mobile ? "mobile_" : ""}form ${
+                  fade ? "fade" : ""
+                }`}
+                // ref={fadeElement}
+              >
+                <div className="registration_form__title">
+                  <h1 className="title_content">Registration</h1>
+                </div>
+                <div className="registration_form_controls">
+                  <Form.Group className="form_group">
+                    <Form.Label className="in__label">Email</Form.Label>
+                    <Form.Control
+                      required
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      className="registration_form_in"
+                      isValid={touched.email && values.email}
+                      // isInvalid={touched.email && email(values.email)}
+                    />
+                  </Form.Group>
 
-                <Form.Group className="form_group">
-                  <Form.Label className="in__label">Password</Form.Label>
-                  <Form.Control
-                    required
-                    type="password"
-                    name="password"
-                    onChange={handleChange}
-                    className="registration_form_in"
-                    isValid={touched.password && values.password}
-                  />
-                </Form.Group>
-                <Form.Group className="form_group">
-                  <Form.Label className="in__label">Name</Form.Label>
-                  <Form.Control
-                    required
-                    name="name"
-                    onChange={handleChange}
-                    className="registration_form_in"
-                    isValid={touched.name && values.name}
-                  />
-                </Form.Group>
-                <Form.Group className="form_group">
-                  <Form.Label className="in__label">Surname</Form.Label>
-                  <Form.Control
-                    required
-                    name="surname"
-                    onChange={handleChange}
-                    className="registration_form_in"
-                    isValid={touched.surname && values.surname}
-                  />
-                </Form.Group>
-                <Form.Group className="form_group">
-                  <Form.Label className="in__label">Birthday</Form.Label>
-                  <Form.Control
-                    required
-                    type="date"
-                    name="data"
-                    className="form-control"
-                    onChange={handleChange}
-                    // className="registration_form_in"
-                  />
-                </Form.Group>
-                <DnD />
+                  <Form.Group className="form_group">
+                    <Form.Label className="in__label">Password</Form.Label>
+                    <Form.Control
+                      required
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      className="registration_form_in"
+                      isValid={touched.password && values.password}
+                    />
+                  </Form.Group>
+                  <Form.Group className="form_group">
+                    <Form.Label className="in__label">Name</Form.Label>
+                    <Form.Control
+                      required
+                      name="name"
+                      onChange={handleChange}
+                      className="registration_form_in"
+                      isValid={touched.name && values.name}
+                    />
+                  </Form.Group>
+                  <Form.Group className="form_group">
+                    <Form.Label className="in__label">Surname</Form.Label>
+                    <Form.Control
+                      required
+                      name="surname"
+                      onChange={handleChange}
+                      className="registration_form_in"
+                      isValid={touched.surname && values.surname}
+                    />
+                  </Form.Group>
+                  <Form.Group className="form_group">
+                    <Form.Label className="in__label">Birthday</Form.Label>
+                    <Form.Control
+                      required
+                      type="date"
+                      name="data"
+                      className="form-control"
+                      onChange={handleChange}
+                      // className="registration_form_in"
+                    />
+                  </Form.Group>
+                </div>
+                <div>
+                  <Button
+                    type="submit"
+                    className={`registration_form_${
+                      mobile ? "mobile_" : ""
+                    }button_submit`}
+                    onClick={() => {
+                      // if (isValidating) {
+                      setFade(true);
+                      setTimeout(() => {
+                        setPageNumber(2);
+                      }, 1000);
+                      // }
+                    }}
+                  >
+                    Submit
+                  </Button>
+                </div>
               </div>
-              <div>
-                <Button
-                  type="submit"
-                  className={`registration_form_${
-                    mobile ? "mobile_" : ""
-                  }button_submit`}
-                >
-                  Submit
-                </Button>
-              </div>
-            </div>
+            ) : (
+              <DnD />
+            )}
           </Form.Group>
           <div className="col-4"></div>
         </Form>
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,44 +1,99 @@
-import React from "react";
+import {
+  faArrowAltCircleUp,
+  faClipboard,
+  faEraser,
+  faMoneyCheck,
+  faTimes,
+  faTimesCircle,
+  faWindowClose,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Input } from "@material-ui/core";
+import { Modal } from "react-bootstrap";
+import { Field } from "formik";
+import React, { useState } from "react";
+import LargePhotosPreview from "../Messanger/LargePhotosPreview";
+import PhotoPreviewModal from "../Messanger/PhotoPreviewModal";
 import "./Profile.scss";
-import ava from "./../../images/ava.png";
 
 const Profile = ({
-  user_ava = "https://instamir.info/wp-content/uploads/2019/04/instami-avatarka-v-instagram-8.png",
+  user_ava = "https://mir-avatarok.3dn.ru/_si/0/84829236.jpg",
 }) => {
-  function TellPIPI(a) {
-    alert("sdjJKSGFJDILKGf");
-  }
+  const [show, setShow] = useState(false);
+  const photo = "https://mir-avatarok.3dn.ru/_si/0/84829236.jpg";
+  const [mouseOnChild, setMouseOnChild] = useState(false);
+
   return (
     <div id="global_profile_container">
       <div className="row">
-        <div className="col-6">
-          <img src={user_ava} alt="profile_avatar" className="profile_avatar" />
-          <div
-            onClick={() => {
-              TellPIPI(6);
-            }}
-          >
-            Name Surname
+        <div className="profile_avatar_edit col-4">
+          <div className="avatar_container_g">
+            <div className="avatar_container">
+              <img
+                src={user_ava}
+                alt="profile_avatar"
+                className="avatar"
+                onClick={() => setShow(true)}
+              />
+              <div
+                className="down"
+                onClick={() => {
+                  if (!mouseOnChild) {
+                    setShow(true);
+                  }
+                }}
+              >
+                <div className="photo_delete_icon">
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    size="1x"
+                    // color="grey"
+                    onMouseEnter={() => setMouseOnChild(true)}
+                    onMouseLeave={() => setMouseOnChild(false)}
+                  />
+                </div>
+                <label
+                  htmlFor="update_photo"
+                  onMouseEnter={() => setMouseOnChild(true)}
+                  onMouseLeave={() => setMouseOnChild(false)}
+                >
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faArrowAltCircleUp}
+                      size="1x"
+                      style={{ marginRight: "10px" }}
+                    />
+                    <span>Edit photo</span>
+                  </div>
+                </label>
+                <input
+                  id="update_photo"
+                  type="file"
+                  name="update_photo"
+                  onChange={(e) => {
+                    console.log(e);
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-        <R funR={TellPIPI} />
-        <div className="col-6">
+        <div className="col-8">
           <div>ewdf</div>
         </div>
       </div>
+      <ProfileAvaPreview show={show} setShow={setShow} photo={photo} />
     </div>
   );
 };
 
-const R = ({ funR }) => {
+const ProfileAvaPreview = ({ show, setShow, photo }) => {
   return (
-    <div
-      onClick={() => {
-        funR();
-      }}
-    >
-      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa
-    </div>
+    <Modal show={show} onHide={() => setShow(false)}>
+      <div className="prt">
+        <img src={photo} alt="" />
+      </div>
+    </Modal>
   );
 };
 
