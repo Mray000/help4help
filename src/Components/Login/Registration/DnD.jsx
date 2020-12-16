a3ec07a073da19c93007e32970a4a404931ff9de
Mray000 <ainurhabibullin0@gmail.com>
2020-12-16 23:54:01 +0300

    dnd_styling

--- src/Components/Login/Registration/DnD.jsx
+++ src/Components/Login/Registration/DnD.jsx
@@ -1,9 +1,10 @@
+import { Button } from "@material-ui/core";
 import React, { useState } from "react";
 
-const DnD = () => {
+const DnD = ({ fade, setFade, setPageNumber }) => {
   const [LessonsForHelping, setLessonsForHelping] = useState([]);
   const [LessonsForLearning, setLessonsForLearning] = useState([]);
-  const [lessons, setLessons] = useState([
+  const lessons = [
     { id: 1, lesson: "Math", img: "" },
     { id: 2, lesson: "Lang", img: "" },
     { id: 3, lesson: "Phith", img: "" },
@@ -14,58 +15,65 @@ const DnD = () => {
     { id: 8, lesson: "PE", img: "" },
     { id: 9, lesson: "History", img: "" },
     { id: 10, lesson: "Social", img: "" },
-  ]);
+    { id: 11, lesson: "English", img: "" },
+    { id: 12, lesson: "Music", img: "" },
+  ];
+  const draggable_elements = [3, 6, 9, 12];
   const DragStart = (ev, e) => {
-    console.log("data start...");
-    ev.dataTransfer.setData("text", e);
+    ev.dataTransfer.setData("id", e.id);
   };
   const DragOver = (ev) => {
     ev.preventDefault();
   };
   const Drop = (ev, hl) => {
     ev.preventDefault();
-    let DraggingElData = ev.dataTransfer.getData("text");
+    let DraggingEl = lessons.find(
+      (l) => l.id === Number(ev.dataTransfer.getData("id"))
+    );
     let massForHL = hl === "h" ? LessonsForHelping : LessonsForLearning;
     let setMassForFilter =
       hl === "h" ? setLessonsForLearning : setLessonsForHelping;
     let setMassForHL =
       hl === "h" ? setLessonsForHelping : setLessonsForLearning;
-    if (massForHL.length < 5 && !massForHL.find((i) => i === DraggingElData)) {
+    if (
+      hl !== "r" &&
+      DraggingEl &&
+      massForHL.length < 6 &&
+      !massForHL.find((e) => e.id === DraggingEl.id)
+    ) {
       setMassForFilter((lastData) => {
-        return lastData.filter((i) => i !== DraggingElData);
+        return lastData.filter((e) => e.id !== DraggingEl.id);
       });
       setMassForHL((lastData) => {
-        return [...lastData, DraggingElData];
+        return [...lastData, DraggingEl];
       });
     }
     if (hl === "r") {
-      setLessonsForHelping((lastData) => {
-        return lastData.filter((e) => e !== DraggingElData);
-      });
-      setLessonsForLearning((lastData) => {
-        return lastData.filter((e) => e !== DraggingElData);
-      });
+      if (DraggingEl) {
+        setLessonsForHelping((lastData) => {
+          return lastData.filter((e) => e.id !== DraggingEl.id);
+        });
+        setLessonsForLearning((lastData) => {
+          return lastData.filter((e) => e.id !== DraggingEl.id);
+        });
+      }
     }
   };
   const DraggableElement = ({ e }) => {
     return (
       <div
         className={`draggable ${
-          LessonsForHelping.find((i) => i === e.lesson) ||
-          LessonsForLearning.find((i) => i === e.lesson)
+          LessonsForHelping.find((i) => i.id === e.id) ||
+          LessonsForLearning.find((i) => i.id === e.id)
             ? "dragged"
             : null
         }`}
         draggable={`${
-          !LessonsForHelping.find((i) => i === e.lesson) &&
-          !LessonsForLearning.find((i) => i === e.lesson)
+          !LessonsForHelping.find((i) => i.id === e.id) &&
+          !LessonsForLearning.find((i) => i.id === e.id)
         }`}
         onDragStart={(ev) => {
-          DragStart(ev, e.lesson);
-        }}
-        style={{
-          marginRight:
-            lessons.indexOf(e) === 4 || lessons.indexOf(e) === 9 ? "0" : "",
+          DragStart(ev, e);
         }}
       >
         <span>{e.lesson}</span>
@@ -75,41 +83,73 @@ const DnD = () => {
 
   const DroppableElement = ({ hl }) => {
     let massForLH = hl === "h" ? LessonsForHelping : LessonsForLearning;
+    let massForElContainer1 = massForLH.slice(0, 3);
+    let massForElContainer2 = massForLH.slice(3, 6);
     return (
-      <>
-        <span>
-          {hl === "h" ? "What you know very good?" : "What you want learn?"}{" "}
+      <div className="droppable">
+        <span className="droppable_t">
+          {hl === "h" ? "What you know very good?" : "What you want learn?"}
         </span>
-        <div
-          className="droppable"
-          onDragOver={(ev) => {
-            DragOver(ev);
-          }}
-          onDrop={(ev) => {
-            Drop(ev, hl);
-          }}
-        >
-          {massForLH.map((e) => {
-            return (
-              <div
-                draggable="true"
-                className="draggable"
-                style={{
-                  borderRight: massForLH.indexOf(e) === 4 ? "none" : "",
-                }}
-                onDragStart={(ev) => DragStart(ev, e)}
-              >
-                {e}
-              </div>
-            );
-          })}
+        <div className="droppable_e_g_container">
+          <div
+            className="droppable_e_container_1"
+            onDragOver={(ev) => {
+              DragOver(ev);
+            }}
+            onDrop={(ev) => {
+              Drop(ev, hl);
+            }}
+          >
+            {massForElContainer1.map((e) => {
+              return (
+                <div
+                  draggable="true"
+                  className="draggable"
+                  style={{
+                    borderRight:
+                      massForElContainer1.indexOf(e) === 2 ? "none" : "",
+                  }}
+                  onDragStart={(ev) => DragStart(ev, e)}
+                >
+                  {e.lesson}
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className="droppable_e_container_2"
+            onDragOver={(ev) => {
+              DragOver(ev);
+            }}
+            onDrop={(ev) => {
+              Drop(ev, hl);
+            }}
+          >
+            {massForElContainer2.map((e) => {
+              return (
+                <div
+                  draggable="true"
+                  className="draggable"
+                  style={{
+                    borderRight:
+                      massForElContainer2.indexOf(e) === 2 ? "none" : "",
+                    borderBottom: "none",
+                  }}
+                  onDragStart={(ev) => DragStart(ev, e)}
+                >
+                  {e.lesson}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </>
+      </div>
     );
   };
 
   return (
-    <div className="lessons_choice_container">
+    <div className={`lessons_choice_container ${!fade ? "fade" : ""}`}>
+      <div className="tittle">Сhoose a lessons</div>
       <section
         className="draggable_elements"
         onDragOver={(ev) => {
@@ -119,25 +159,35 @@ const DnD = () => {
           Drop(ev, "r");
         }}
       >
-        <div className="draggable_elements_1">
-          {lessons
-            .filter((e) => e.id <= 5)
-            .map((e) => {
-              return <DraggableElement e={e} />;
-            })}
-        </div>
-        <div className="draggable_elements_2">
-          {lessons
-            .filter((e) => e.id > 5 && e.id <= 10)
-            .map((e) => {
-              return <DraggableElement e={e} />;
-            })}
-        </div>
+        {draggable_elements.map((i) => {
+          return (
+            <div className="draggable_elements_i">
+              {lessons
+                .filter((e) => e.id > i - 3 && e.id <= i)
+                .map((e) => {
+                  return <DraggableElement e={e} />;
+                })}
+            </div>
+          );
+        })}
       </section>
       <section className="droppable_elements">
         <DroppableElement hl="h" />
         <DroppableElement hl="l" />
       </section>
+      <div className="buttons_container">
+        <Button
+          onClick={() => {
+            setFade(false);
+            setTimeout(() => {
+              setPageNumber(1);
+            }, 1000);
+          }}
+        >
+          Go back
+        </Button>
+        <Button>Submit</Button>
+      </div>
     </div>
   );
 };
--- src/Components/Login/Registration/Registration.jsx
+++ src/Components/Login/Registration/Registration.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Formik } from "formik";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Redirect } from "react-router-dom";
 import "./Registration.scss";
 import DnD from "./DnD";
+import { Button } from "@material-ui/core";
 
 const Registration = () => {
   const mobile = false;
@@ -42,84 +41,83 @@ const Registration = () => {
                 <div className="registration_form__title">
                   <h1 className="title_content">Registration</h1>
                 </div>
-                <div className="registration_form_controls">
-                  <Form.Group className="form_group">
-                    <Form.Label className="in__label">Email</Form.Label>
-                    <Form.Control
-                      required
-                      type="email"
-                      name="email"
-                      onChange={handleChange}
-                      className="registration_form_in"
-                      isValid={touched.email && values.email}
-                      // isInvalid={touched.email && email(values.email)}
-                    />
-                  </Form.Group>
+                <Form.Group className="form_group">
+                  <Form.Label className="in__label">Email</Form.Label>
+                  <Form.Control
+                    required
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    className="registration_form_in"
+                    isValid={touched.email && values.email}
+                  />
+                </Form.Group>
 
-                  <Form.Group className="form_group">
-                    <Form.Label className="in__label">Password</Form.Label>
-                    <Form.Control
-                      required
-                      type="password"
-                      name="password"
-                      onChange={handleChange}
-                      className="registration_form_in"
-                      isValid={touched.password && values.password}
-                    />
-                  </Form.Group>
-                  <Form.Group className="form_group">
-                    <Form.Label className="in__label">Name</Form.Label>
-                    <Form.Control
-                      required
-                      name="name"
-                      onChange={handleChange}
-                      className="registration_form_in"
-                      isValid={touched.name && values.name}
-                    />
-                  </Form.Group>
-                  <Form.Group className="form_group">
-                    <Form.Label className="in__label">Surname</Form.Label>
-                    <Form.Control
-                      required
-                      name="surname"
-                      onChange={handleChange}
-                      className="registration_form_in"
-                      isValid={touched.surname && values.surname}
-                    />
-                  </Form.Group>
-                  <Form.Group className="form_group">
-                    <Form.Label className="in__label">Birthday</Form.Label>
-                    <Form.Control
-                      required
-                      type="date"
-                      name="data"
-                      className="form-control"
-                      onChange={handleChange}
-                      // className="registration_form_in"
-                    />
-                  </Form.Group>
-                </div>
-                <div>
-                  <Button
-                    type="submit"
-                    className={`registration_form_${
-                      mobile ? "mobile_" : ""
-                    }button_submit`}
-                    onClick={() => {
-                      // if (isValidating) {
-                      setFade(true);
-                      setTimeout(() => {
-                        setPageNumber(2);
-                      }, 1000);
-                      // }
-                    }}
-                  >
-                    Submit
-                  </Button>
-                </div>
+                <Form.Group className="form_group">
+                  <Form.Label className="in__label">Password</Form.Label>
+                  <Form.Control
+                    required
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    className="registration_form_in"
+                    isValid={touched.password && values.password}
+                  />
+                </Form.Group>
+                <Form.Group className="form_group">
+                  <Form.Label className="in__label">Name</Form.Label>
+                  <Form.Control
+                    required
+                    name="name"
+                    onChange={handleChange}
+                    className="registration_form_in"
+                    isValid={touched.name && values.name}
+                  />
+                </Form.Group>
+                <Form.Group className="form_group">
+                  <Form.Label className="in__label">Surname</Form.Label>
+                  <Form.Control
+                    required
+                    name="surname"
+                    onChange={handleChange}
+                    className="registration_form_in"
+                    isValid={touched.surname && values.surname}
+                  />
+                </Form.Group>
+                <Form.Group className="form_group">
+                  <Form.Label className="in__label">Birthday</Form.Label>
+                  <Form.Control
+                    required
+                    type="date"
+                    name="data"
+                    className="form-control"
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Button
+                  type="submit"
+                  className={`registration_form_${
+                    mobile ? "mobile_" : ""
+                  }button_submit`}
+                  onClick={() => {
+                    // if (isValidating) {
+                    setFade(true);
+                    setTimeout(() => {
+                      setPageNumber(2);
+                    }, 1000);
+                    // }
+                  }}
+                >
+                  Submit
+                </Button>
               </div>
             ) : (
-              <DnD />
+              <DnD
+                fade={fade}
+                setFade={setFade}
+                setPageNumber={setPageNumber}
+              />
+              // <div>a</div>
             )}
           </Form.Group>
           <div className="col-4"></div>
